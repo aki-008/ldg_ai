09474e24d42ee1ee1bdcb75e0833677e1762ecb9
aki-008 <shaswatsavage@gmail.com>
2025-12-24 22:49:11 +0530

    chore: minor auth fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,13 +26,15 @@ app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
 
 
 origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # WARNING: Only for development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
