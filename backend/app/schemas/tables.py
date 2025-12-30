76974d964016acfcc827873edc8ae309cf3365c9
aki-008 <shaswatsavage@gmail.com>
2025-12-30 23:59:41 +0530

    feat: dir db setup

--- backend/app/schemas/tables.py
+++ backend/app/schemas/tables.py
@@ -1,5 +1,7 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, LargeBinary, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from typing import List
 from app.db.session import Base
 
 
@@ -10,3 +12,33 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255))
+
+    case_dir: Mapped[list["Case_dir"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Case_dir(Base):
+    __tablename__ = "case_dir"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    dir_name: Mapped[str] = mapped_column(String(255))
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="case_dir")
+    files: Mapped[List["File"]] = relationship(
+        back_populates="case_dir", cascade="all, delete-orphan"
+    )
+
+
+class File(Base):
+    __tablename__ = "files"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    filename: Mapped[str] = mapped_column(String(255))
+    file_data: Mapped[bytes] = mapped_column(LargeBinary)
+    uploaded_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+
+    case_dir_id: Mapped[int] = mapped_column(ForeignKey("case_dirs.id"))
+
+    case_dir: Mapped["Case_dir"] = relationship(back_populates="files")
